ca53234adad22cbe006f76fa11acbf8029010abd
Anish Karthik <89824626+anishfish2@users.noreply.github.com>
2022-10-16 08:44:03 -0500

    color inference

--- inferenceColor.py
+++ inferenceColor.py
@@ -22,7 +22,8 @@ import imgproc
 import model
 from utils import load_state_dict
 import shutil
-
+import numpy as np
+import matplotlib.pyplot as plt
 model_names = sorted(
     name for name in model.__dict__ if
     name.islower() and not name.startswith("__") and callable(model.__dict__[name]))
@@ -98,7 +99,7 @@ def main(args):
     print(time.perf_counter() - timestart)
     print(f"SR image save to {args.output_path}")
 
-resFile = 'res2'
+resFile = 'res'
 resFolderPath = os.path.join(os.getcwd(), "figure", "video_files", resFile)
 if os.path.exists(resFolderPath):
     shutil.rmtree(resFolderPath)
@@ -111,15 +112,53 @@ def convert_frames_to_video(pathIn,pathOut,fps):
     
     for i in range(len([entry for entry in os.listdir(pathIn) if os.path.isfile(os.path.join(pathIn, entry))])):
         filename = f"{pathIn}/{resFile}_{i}.png"
-        print(filename)
+        print(i)
         #reading each files
         img = cv2.imread(filename)
         
         height, width, layers = img.shape
         size = (width,height)
+        prototxt = "./colorization_deploy_v2.prototxt"
+        caffe_model = "./colorization_release_v2.caffemodel"
+        pts_npy = "./pts_in_hull.npy"
+        test_image =  filename
+
+        net = cv2.dnn.readNetFromCaffe(prototxt, caffe_model)
+        pts = np.load(pts_npy)
         
+        layer1 = net.getLayerId("class8_ab")
+        #print(layer1)
+        layer2 = net.getLayerId("conv8_313_rh")
+        #print(layer2)
+        pts = pts.transpose().reshape(2, 313, 1, 1)
+        net.getLayer(layer1).blobs = [pts.astype("float32")]
+        net.getLayer(layer2).blobs = [np.full([1, 313], 2.606, dtype="float32")]
+
+        test_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        test_image = cv2.cvtColor(test_image, cv2.COLOR_GRAY2RGB)
+
+        normalized = test_image.astype("float32") / 255.0
+        lab_image = cv2.cvtColor(normalized, cv2.COLOR_RGB2LAB)
+        resized = cv2.resize(lab_image, (224, 224))
+        L = cv2.split(resized)[0]
+        L -= 50
+
+        net.setInput(cv2.dnn.blobFromImage(L))
+        ab = net.forward()[0, :, :, :].transpose((1, 2, 0))
+        ab = cv2.resize(ab, (test_image.shape[1], test_image.shape[0]))
+        L = cv2.split(lab_image)[0]
+        LAB_colored = np.concatenate((L[:, :, np.newaxis], ab), axis=2)
+        RGB_colored = cv2.cvtColor(LAB_colored,cv2.COLOR_LAB2RGB)
+        RGB_colored = np.clip(RGB_colored, 0, 1)
+        RGB_colored = (255 * RGB_colored).astype("uint8")
+        # if (i % 5 == 0):
+        #     plt.imshow(RGB_colored)
+        #     plt.title('Colored Image')
+        #     plt.show()
+        RGB_BGR = cv2.cvtColor(RGB_colored, cv2.COLOR_RGB2BGR)
+
         #inserting the frames into an image array
-        frame_array.append(img)
+        frame_array.append(RGB_BGR)
     out = cv2.VideoWriter(pathOut,cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
     for i in range(len(frame_array)):
         # writing to a image array
